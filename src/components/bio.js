35e2eef0909ca51d59bd4aafc3f75b55e2d6f320
hsoyier <reiyoshizawa0418@gmail.com>
2020-04-12 12:58:55 -0700

    style: disable typography

--- src/components/bio.js
+++ src/components/bio.js
@@ -9,8 +9,6 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
 
-import { rhythm } from "../utils/typography"
-
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
@@ -37,12 +35,7 @@ const Bio = () => {
 
   const { author, social } = data.site.siteMetadata
   return (
-    <div
-      style={{
-        display: `flex`,
-        marginBottom: rhythm(2.5),
-      }}
-    >
+    <div>
       <Image fixed={data.avatar.childImageSharp.fixed} alt={author.name} />
       <p>
         Written by <strong>{author.name}</strong> {author.summary}
